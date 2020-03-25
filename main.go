2ecc67a661acd3aff17e2ef94951588c99f62979
hua <1901210745@pku.edu.cn>
2020-03-25 16:10:23 +0800

    将goadmin全局配置写到config.json文件中

--- main.go
+++ main.go
@@ -5,13 +5,11 @@ import (
 	"github.com/GoAdminGroup/go-admin/engine"
 	"github.com/GoAdminGroup/go-admin/modules/config"
 	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/mysql" // 引入对应数据库引擎
-	"github.com/GoAdminGroup/go-admin/modules/language"
 	"github.com/GoAdminGroup/go-admin/plugins/admin"
 	_ "github.com/GoAdminGroup/themes/adminlte" // 引入主题，必须引入，不然报错
 	"ncov-report-manage-system-GO/model"
 	"ncov-report-manage-system-GO/server"
 )
-
 
 func main() {
 	r := server.Router()
@@ -19,28 +17,30 @@ func main() {
 	// 实例化一个GoAdmin引擎对象
 	eng := engine.Default()
 
-	// GoAdmin全局配置，也可以写成一个json，通过json引入
-	cfg := config.Config{
-		Databases: config.DatabaseList{
-			"default": {
-				Host:       "127.0.0.1",
-				Port:       "3306",
-				User:       "",
-				Pwd:        "",
-				Name:       "",
-				MaxIdleCon: 50,
-				MaxOpenCon: 150,
-				Driver:     config.DriverMysql,
-			},
-		},
-		UrlPrefix: "ncov", // 访问网站的前缀
-		// Store 必须设置且保证有写权限，否则增加不了新的管理员用户
-		Store: config.Store{
-			Path:   "./uploads",
-			Prefix: "uploads",
-		},
-		Language: language.CN,
-	}
+	cfg := config.ReadFromJson("./config.json")
+
+	//// GoAdmin全局配置，也可以写成一个json，通过json引入
+	//cfg := config.Config{
+	//	Databases: config.DatabaseList{
+	//		"default": {
+	//			Host:       "127.0.0.1",
+	//			Port:       "3306",
+	//			User:       "",
+	//			Pwd:        "",
+	//			Name:       "",
+	//			MaxIdleCon: 50,
+	//			MaxOpenCon: 150,
+	//			Driver:     config.DriverMysql,
+	//		},
+	//	},
+	//	UrlPrefix: "ncov", // 访问网站的前缀
+	//	// Store 必须设置且保证有写权限，否则增加不了新的管理员用户
+	//	Store: config.Store{
+	//		Path:   "./uploads",
+	//		Prefix: "uploads",
+	//	},
+	//	Language: language.CN,
+	//}
 
 	// 这里引入你需要管理的业务表配置
 	adminPlugin := admin.NewAdmin(model.Generators)
